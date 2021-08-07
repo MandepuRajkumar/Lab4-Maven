daf4654fff533034c632b5182ed264e6ed0e7d4d
MandepuRajkumar <66173215+MandepuRajkumar@users.noreply.github.com>
2021-08-07 09:38:42 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-  public String sayHello() {
+  public String sayHello {
     return "Hello world- This is Venkata Naresh Mandhala your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
 }
